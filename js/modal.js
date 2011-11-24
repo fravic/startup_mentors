74052e11bce37c589ecad18d9d80a732588f0f46
Hoon Cho <soohoon90@gmail.com>
2011-11-24 00:12:59 -0500

    date picker now gives last monday as starting date and selects a week

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,37 @@
 $(function(){
+    
    	$('#datepicker').datepicker({
 		changeMonth: true,
-		changeYear: true
+		changeYear: true,
+		firstDay: 1,
+		onSelect: function(dateText, inst) {
+		    var d = dateText.split("/");
+		    var myDate=new Date();
+		    myDate.setFullYear(d[2],d[0]-1,d[1]);
+		    var day = myDate.getDay();
+            if( day !== 1 )                // Only manipulate the date if it isn't Mon.
+                myDate.setHours(-24 * (day - 1));   // Set the hours to day number minus 1
+            
+	    }
 	});
+	
+	$( "#datepicker" ).datepicker().click(function(event) {
+        // highlight the TR
+        $(".ui-datepicker-current-day").parent().addClass('highlight');
+        // highlight the TD > A
+        $(".ui-datepicker-current-day").siblings().find('a').addClass('white');
+    }); 
+	
     $('#newEntryModal').modal({
       backdrop: true,
       keyboard: true
     });
+    
     $('#newRequestModal').modal({
       backdrop: true,
       keyboard: true
     });
+    
     $('#newEntryButton').click(function () {
         var entry = {
           Type: "Entry",
@@ -24,10 +45,7 @@ $(function(){
         $("#textarea").val("");
         $('#newEntryModal').modal('hide');
     });
-    
-    console.log(feed);
-    console.log(feed.categories);
-    
+        
     var options = '';
     for (var i = 0; i < feed.categories.length; i++) {
         options += '<option value="' +  feed.categories[i] + '">' +  feed.categories[i] + '</option>';
