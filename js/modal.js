1a0b3d7cba43d23586e2e5afc54d2ac69546c6de
Hoon Cho <soohoon90@gmail.com>
2011-11-24 00:36:27 -0500

    module and move move move

--- js/modal.js
+++ js/modal.js
@@ -11,8 +11,35 @@ $(function(){
 		    var day = myDate.getDay();
             if( day !== 1 )                // Only manipulate the date if it isn't Mon.
                 myDate.setHours(-24 * (day - 1));   // Set the hours to day number minus 1
+            var i=0;
+            html = "<h4>Select up to 10 time slots for meeting:</h4>"
+            html += '<table class="times">';
+            html += '<tr><th class="info"></th></tr>';
+            for (i=9;i<=12;i++){
+                html += '<tr><th class="info">'+i+'</th></tr>';
+            }
+            for (i=1;i<=5;i++){
+                html += '<tr><th class="info">'+i+'</th></tr>';
+            }
+            html += '</table><table class="timepicker"><tr>';
             
-	    }
+            for (i=0;i<5;i++){
+                html+= '<td class="info">'+ myDate.getDate() +'</td>'
+                myDate.setDate(myDate.getDate()+1);
+            }
+            
+            html += '</tr>';
+            
+            for (i=1;i<=18;i++){
+                html += '<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>';
+            }
+            
+            html += '<table class="selections"><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr><tr><td></td><td></td></tr></table>';
+            
+            console.log(html);
+            // $("#timepicker").html(html);
+            $("#timetest").html(html);
+            }
 	});
 	
 	$( "#datepicker" ).datepicker().click(function(event) {
